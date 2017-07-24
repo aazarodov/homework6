cc5df60cfb91972e7a547f9180bf0c737415e042
aazarodov <aazarodov@gmail.com>
2017-07-24 11:27:40 +0300

    java3 homework6

--- src/main/java/homework6/Tasks.java
+++ src/main/java/homework6/Tasks.java
@@ -1,6 +1,18 @@
 package homework6;
 
 public class Tasks {
+    /**
+     * Написать метод, которому в качестве аргумента передается не пустой одномерный
+     * целочисленный массив, метод должен вернуть новый массив, который получен путем
+     * вытаскивания элементов из исходного массива, идущих после последней четверки. Входной
+     * массив должен содержать хотя бы одну четверку, в противном случае в методе необходимо
+     * выбросить RuntimeException.
+     * Написать набор тестов для этого метода (варианта 3-4 входных данных)
+     * вх: [ 1 2 4 4 2 3 4 1 7 ] -> вых: [ 1 7 ]
+     * @param array
+     * @return
+     * @throws RuntimeException
+     */
     public Integer[] task1 (Integer[] array) throws RuntimeException {
         Integer[] result = null;
         int maxIndex = -1;
@@ -21,6 +33,13 @@ public class Tasks {
         return result;
     }
 
+    /**
+     * Написать метод, который проверяет что массив состоит только из чисел 1 и 4. Если в массиве
+     * нет хоть одной 4 или 1, то метод вернет false;
+     * Написать набор тестов для этого метода (варианта 3-4 входных данных)
+     * @param array
+     * @return
+     */
     public boolean task2(Integer[] array) {
         boolean containsFour = false, containsOne = false, containsOther = false;
         if (array != null && array.length > 0) {
